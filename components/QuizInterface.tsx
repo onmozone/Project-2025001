e89dcf2f403dc5604cfb81e51fb179de728e3390
itsMHmoon <mhmoon.info@gmail.com>
2026-01-09 00:46:22 +0600

    20260109-01

--- components/QuizInterface.tsx
+++ components/QuizInterface.tsx
@@ -297,7 +297,7 @@ const QuizInterface: React.FC<QuizInterfaceProps> = ({ user, questionSet, onFini
                     </div>
 
                     {hasScroll && (
-                      <div className="fixed bottom-[13vh] right-[20vw] bg-black text-white px-5 py-2.5 rounded-md shadow-lg flex items-center gap-2 text-xs font-semibold animate-bounce cursor-default select-none z-10">
+                      <div className="fixed bottom-[13vh] right-[20.5vw] bg-black text-white px-5 py-2.5 rounded-md shadow-lg flex items-center gap-2 text-xs font-semibold animate-bounce cursor-default select-none z-10">
                         <span>এই পৃষ্ঠাটির স্ক্রলিং প্রয়োজন</span>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.0" stroke="currentColor" class="size-4">
                           <path stroke-linecap="round" stroke-linejoin="round" d="m9 12.75 3 3m0 0 3-3m-3 3v-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
